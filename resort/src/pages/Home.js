894dc53fffd4ac2780fd8ff229307319c80f8361
Saba Shavidze <shavidze>
2019-05-14 18:02:03 +0400

    import external file sources

---
+++ resort/src/pages/Home.js
@@ -0,0 +1,17 @@
+const Home = () => {
+    return (div.a);
+}
+
+export default Home;
+import React from 'react'
+
+const Home = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Home;
