5e31f7c78eba531a45510e5e2157ecb190739a48
Saba Shavidze <shavidze>
2019-05-15 14:52:00 +0400

    resort: added navbar and hero components, create routes, installed some little packages: react-icons, react-router-dom

--- resort/src/pages/Error.js
+++ resort/src/pages/Error.js
@@ -1,9 +1,7 @@
 import React from "react"
-
+import Hero from "../components/Hero"
 export default function Error() {
     return (
-        <div>
-            error page
-        </div>
+        <Hero />
     )
 }
--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Hero from "../components/Hero"
 
 
 const Rooms = () => {
-    return (<div>Rooms Page</div>);
+    console.log("aqaa")
+    return <Hero hero="roomsHero" />;
 }
 
 export default Rooms;
