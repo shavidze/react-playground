e9d150a91b2fcb237b7640cac880b1d1cdf1927e
Saba Shavidze <shavidze>
2019-05-27 18:13:04 +0400

    changes some small issues

--- resort/src/pages/SingleRoom.js
+++ resort/src/pages/SingleRoom.js
@@ -52,7 +52,7 @@ class SingleRoom extends Component {
             <article className="info">
               <h3>info</h3>
               <h6>price : ${price}</h6>
-              <h6>size : ${size}SQFT</h6>
+              <h6>size : {size}SQFT</h6>
               <h6>max capacity: {capacity > 1 ? `${capacity} people` : `${capacity} person`}</h6>
               <h6>{pets ? 'pets allowed' : 'no pets'}</h6>
               <h6>{breakfast && 'free breakfast included'}</h6>
