2f871e08744aa7e6c54241e11408470de0c5387d
saba <sabashavidze@gmail.com>
2019-06-04 00:25:07 +0400

    Updates

---
+++ precache-manifest.915de45d19678d387ae2ce268891da49.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc1f24c618984a2c0fcc2d96ef73cbf",
+    "url": "/react-playground/tree/master/resort/index.html"
+  },
+  {
+    "revision": "430a69311df90c8689e6",
+    "url": "/react-playground/tree/master/resort/static/css/main.2f01f02f.chunk.css"
+  },
+  {
+    "revision": "341f71e2558e69921ce2",
+    "url": "/react-playground/tree/master/resort/static/js/2.33198e6b.chunk.js"
+  },
+  {
+    "revision": "430a69311df90c8689e6",
+    "url": "/react-playground/tree/master/resort/static/js/main.2ca84531.chunk.js"
+  },
+  {
+    "revision": "5776d9c4f062e058fa4b",
+    "url": "/react-playground/tree/master/resort/static/js/runtime~main.2be1b75e.js"
+  },
+  {
+    "revision": "aebaec51b2f82a2993fda821ca04e228",
+    "url": "/react-playground/tree/master/resort/static/media/defaultBcg.aebaec51.jpeg"
+  },
+  {
+    "revision": "33ab9f1e88f204b5b8a13725a918f36c",
+    "url": "/react-playground/tree/master/resort/static/media/details-1.33ab9f1e.jpeg"
+  },
+  {
+    "revision": "50c23e6265e40d602f1ac080e2d4169a",
+    "url": "/react-playground/tree/master/resort/static/media/details-2.50c23e62.jpeg"
+  },
+  {
+    "revision": "7345b9f61680c799868f66f297cedab5",
+    "url": "/react-playground/tree/master/resort/static/media/details-3.7345b9f6.jpeg"
+  },
+  {
+    "revision": "7569a31606be3db6b229327eec5999df",
+    "url": "/react-playground/tree/master/resort/static/media/details-4.7569a316.jpeg"
+  },
+  {
+    "revision": "e9ea73949de71d45ae46047646b95727",
+    "url": "/react-playground/tree/master/resort/static/media/loading-arrow.e9ea7394.gif"
+  },
+  {
+    "revision": "9f0cccc1e406a5aa9ef6ee36229762c3",
+    "url": "/react-playground/tree/master/resort/static/media/logo.9f0cccc1.svg"
+  },
+  {
+    "revision": "e928a5c462a9f4f590af64285b387571",
+    "url": "/react-playground/tree/master/resort/static/media/room-1.e928a5c4.jpeg"
+  },
+  {
+    "revision": "3b5c08716be6b713154c8aedaec3f5bf",
+    "url": "/react-playground/tree/master/resort/static/media/room-10.3b5c0871.jpeg"
+  },
+  {
+    "revision": "f08fee37bdc8f3d11ef36658cfacc44e",
+    "url": "/react-playground/tree/master/resort/static/media/room-11.f08fee37.jpeg"
+  },
+  {
+    "revision": "0e293352532d9483616fe4c5110c8e28",
+    "url": "/react-playground/tree/master/resort/static/media/room-12.0e293352.jpeg"
+  },
+  {
+    "revision": "052c76815f889f20b8a498c19a212c50",
+    "url": "/react-playground/tree/master/resort/static/media/room-2.052c7681.jpeg"
+  },
+  {
+    "revision": "9a1103b597895b4c4b373d6304400781",
+    "url": "/react-playground/tree/master/resort/static/media/room-3.9a1103b5.jpeg"
+  },
+  {
+    "revision": "1e407d88a352a0e8493e2275a76f39f5",
+    "url": "/react-playground/tree/master/resort/static/media/room-4.1e407d88.jpeg"
+  },
+  {
+    "revision": "a5c06e0bb3e90f927159439d68b07253",
+    "url": "/react-playground/tree/master/resort/static/media/room-5.a5c06e0b.jpeg"
+  },
+  {
+    "revision": "b9a34e969ab86c1500d67183f646995a",
+    "url": "/react-playground/tree/master/resort/static/media/room-6.b9a34e96.jpeg"
+  },
+  {
+    "revision": "e251c8d571a7f753b3da985e4b8a34c6",
+    "url": "/react-playground/tree/master/resort/static/media/room-7.e251c8d5.jpeg"
+  },
+  {
+    "revision": "ebb7fca5f456fb07e0096ab3ef72b330",
+    "url": "/react-playground/tree/master/resort/static/media/room-8.ebb7fca5.jpeg"
+  },
+  {
+    "revision": "7b17c49f01aa17fc11df8400d604e1c9",
+    "url": "/react-playground/tree/master/resort/static/media/room-9.7b17c49f.jpeg"
+  }
+]);
